36ff3503eb813f829f7f2e8f45823014786fce55
Kevin McKee <kevin.mckee@procore.com>
2023-02-09 13:12:58 -0800

    Adding Eureka as dependency

--- Package.swift
+++ Package.swift
@@ -5,8 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "ProcoreSwiftPackages",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ProcoreSwiftPackages",
             targets: ["ProcoreSwiftPackages"]
@@ -16,11 +18,10 @@ let package = Package(
         .package(url: "https://github.com/xmartlabs/Eureka.git", exact: "5.3.6")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ProcoreSwiftPackages",
-            dependencies: []),
+            dependencies: ["Eureka"]
+        ),
         .testTarget(
             name: "ProcoreSwiftPackagesTests",
             dependencies: ["ProcoreSwiftPackages"]),
