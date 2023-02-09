5cd4100aa6069773581a213e2fd742b646b02c28
Kevin McKee <kevin.mckee@procore.com>
2023-02-09 12:53:49 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProcoreSwiftPackages
+
+Procore Swift Package dependencies.
---
+++ Sources/ProcoreSwiftPackages/ProcoreSwiftPackages.swift
@@ -0,0 +1,6 @@
+public struct ProcoreSwiftPackages {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
